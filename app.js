c46e1bbc26ebb3fb9c9cb3ba12ff33010388a59a
student <student@Admins-iMac.local>
2020-01-14 14:21:03 -0600

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+
+/* variable that contains my name */
 var myName = 'Hallie';
-
-const states = 50;
-
 console.log(myName);
 
+
+// constant that contains the number of states in the U.S
+const states = 50;
 console.log(states);
+
+
+// result of adding 5 and 4 
+var number = 5 + 4;
+console.log(number);
+
+
+// write a function called sayHello the displays an alert that says Hello World!
+function sayHello (greeting) {
+      alert (greeting); 
+   }                
+sayHello("Hello World!");
+
+
+// checkAge function that checks for name and age
